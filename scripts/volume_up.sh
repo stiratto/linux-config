8185fd99c6ed04439238bfe57542b3490f71c281
stiratto <115959425+stiratto@users.noreply.github.com>
2025-04-05 22:42:31 -0500

    Update volume_up.sh

--- scripts/volume_up.sh
+++ scripts/volume_up.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Obtiene el volumen actual
 
 pactl set-sink-volume @DEFAULT_SINK@ +10%
 
 volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+%' | head -n 1)
-# Muestra la notificación con Dunst
+
 dunstify -a "changeVolume" -t 700 -u low -i audio-volume-high -h string:x-dunst-stack-tag:myvolume "Volume: $volume" 2>/tmp/dunst_error.log
