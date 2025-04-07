63f143d06e135908beb138f18d518f80c75b696f
stiratto <moranmoralesj4@gmail.com>
2025-04-07 10:32:20 -0500

    new script pickcolor
    new script

--- scripts/volume_up.sh
+++ scripts/volume_up.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-
+# get current vol
 pactl set-sink-volume @DEFAULT_SINK@ +10%
 
-volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+%' | head -n 1)
+volume=$(pamixer --get-volume)
 
-dunstify -a "changeVolume" -t 700 -u low -i audio-volume-high -h string:x-dunst-stack-tag:myvolume "Volume: $volume" 2>/tmp/dunst_error.log
+if [[ volume -lt 10 ]]; then
+   icon=""
+elif [[ volume -ge 40 ]]; then
+   icon=""
+else
+   icon=""
+fi
+
+# Muestra la notificación con Dunst
+dunstify -a "changeVolume" -t 700 -h string:x-dunst-stack-tag:myvolume -h int:value:"$volume" "Volume: $volume% " 2>/tmp/dunst_error.log
